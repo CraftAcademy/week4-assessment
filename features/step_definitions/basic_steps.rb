e4306336534ce17a93cf109f07950a022fbb6884
rmzse <rodrigo@therawdelicious.com>
2016-12-04 17:44:16 +0100

    remove all comments witnessing of the long and treacherous struggle

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -8,22 +8,8 @@ end
 
 When(/^I visit the name page for "([^"]*)"$/) do |name|
   visit "/name/#{name}"
-  # visit "/name/#{:name}" 
-  # expect(page).to have_current_path("name/#{name}")
 end
 
 Then(/^I should create a new user with name "([^"]*)"$/) do |name|
-  # expect(User.last.name).to eq name
-  # expect(User.last.name).to_be name
-
-  # This should work
-  user = User.last
-  # expect(user.name).to_be name
-  expect(user.name).to eq name
-
-  # binding.pry
-  # user = User.create(name:params[:"#{name}"])
-  # expect(user.name).to eq = name
-  # # expect(user.name).to_be = name
-  # # # @nameuser = User.create(name: name)
+  expect(User.last.name).to eq name
 end

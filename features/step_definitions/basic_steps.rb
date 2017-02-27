3d50f47924748f841cf61ee9cbe4a393edbea7b7
johan <johan.schantz@telia.com>
2017-02-27 09:00:45 +0100

    delete som shit

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -10,10 +10,6 @@ When(/^I visit the name page for "([^"]*)"$/) do |arg1|
   visit '/name/:Johan'
 end
 
-# Then(/^i should create a new user named "([^"]*)"$/) do |arg1|
-#   expect(name).to have_content name
-# end
-
 Then(/^i should create a new user named "([^"]*)"$/) do |arg1|
   expect(page).to have_content 'Johan'
 end

dcc271572b2f9f8ed1d07c126b413ad32b3918cf
olofvi <olofvi89@gmail.com>
2017-09-16 19:17:48 +0200

    first 3 tests green

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -3,5 +3,13 @@ Given(/^I visit the site$/) do
 end
 
 Then(/^see "([^"]*)"$/) do |name|
-  expect(page).to have_content 
+  expect(page).to have_content
+end
+
+When(/^I visit the name page for "([^"]*)"$/) do |name|
+  visit '/name/Amber'
+end
+
+Then(/^I should create a new user with name "([^"]*)"$/) do |user|
+  expect(page).to have_user
 end
--- features/text.feature
+++ features/text.feature
@@ -3,3 +3,5 @@ Feature: Week 3 assessment
 Scenario: When I visit '/'
 Given I visit the site
 Then see "Ólöf student at Craft Academy"
+When I visit the name page for "Amber"
+Then I should create a new user with name "Amber"

a2948756f5b271c50438ba4690aaec3601c9f6c6
marizaswart <mariza.swart@gmail.com>
2017-11-25 12:46:16 +0100

    Acceptance test and user set up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,16 @@
 class WeekFourAssessmentApp
-  # Your code will go here.
+  get '/' do
+    @name = 'Mariza'
+    erb :welcome
+  end
+
+  get '/name/:name' do
+  "<h1>Welcome #{params[:name]}</h1>"
+  end
+
+ # post '/users' do
+  #  user = User.new(:name)
+  #  user.save
+  #  redirect '/name/:name'
+  #end
 end
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,1 +1,19 @@
-# put step definitions in this file
+Given(/^I visit the site$/) do
+  visit '/'
+end
+
+Then(/^I would like to see "([^"]*)"$/) do |text|
+  expect(page).to have_content text
+end
+
+Then(/^show me the page$/) do
+  save_and_open_page
+end
+
+When(/^I visit the name page for "([^"]*)"$/) do |name|
+  visit '/name/:name'
+end
+
+Then(/^I should create a new user with name "([^"]*)"$/) do |name|
+ fill_in name
+end

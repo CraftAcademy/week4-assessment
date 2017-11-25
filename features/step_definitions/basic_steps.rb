ba500fb7190f1c8aef43b18447b8bd5e4c754c3b
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-11-25 13:47:24 +0100

    Add test Add step definitions Add code in controller Add ruby code in welcome file

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,1 +1,11 @@
-# put step definitions in this file
+Given(/^There is an index page$/) do
+
+end
+
+When(/^I am on the index page$/) do
+  visit '/'
+end
+
+Then(/^I see "([^"]*)"$/) do |arg1|
+   expect(page).to have_content "Zuzanna is a student at Craft Academy"
+end
--- features/text.feature
+++ features/text.feature
@@ -1,2 +1,7 @@
-Feature: Week 4 assessment
+Feature: View Zuzanna on the index page
+
+  Scenario:
+    When I am on the index page
+    Then I see "Zuzanna is a student at Craft Academy"
+
 # Put acceptance tests here - make sure to have a "Feature" and a "Scenario"

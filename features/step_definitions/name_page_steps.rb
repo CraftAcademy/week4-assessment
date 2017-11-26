549f4fae8d067caa6037d45e3c360d2c864ac548
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-11-26 10:45:09 +0100

    Fixing expectation in step definition, all tests pass

--- features/step_definitions/name_page_steps.rb
+++ features/step_definitions/name_page_steps.rb
@@ -4,6 +4,6 @@ end
 
 Then(/^I should create a new user with name "([^"]*)"$/) do |name|
   # expect(user.name).to have_content
-  user1 = User.find_by(name: 'Amber')
-  expect(user1).to exist
+  user1 = User.find_by(name: name)
+  expect(user1.name).to eq 'Amber' 
 end
